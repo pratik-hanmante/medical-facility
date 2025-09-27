390581a9f1a0b6603df0594954d55fbd13cf6932
pratik <pratikhanmante3@gmail.com>
2025-09-27 23:50:48 +0530

    added create billing account method

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,38 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+        try {
+            // Simulated business logic for patient billing account creation
+            String patientId = request.getPatientId();
+            double amount = request.getAmount();
+
+            // In real life, save billing info to DB here
+            System.out.println("Creating billing account for Patient ID: " + patientId + " with amount: " + amount);
+
+            // Build response
+            BillingResponse response = BillingResponse.newBuilder()
+                    .setStatus("SUCCESS")
+                    .setMessage("Billing account created for patient " + patientId)
+                    .setPatientId(patientId)
+                    .setAmount(amount)
+                    .build();
+
+            // Send response
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            // Handle error
+            responseObserver.onError(e);
+        }
+    }
+}
