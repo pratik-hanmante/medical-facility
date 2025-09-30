5dbc89fc7cb1cf57d39d35f0938e018cc9014427
pratik <pratikhanmante3@gmail.com>
2025-09-30 23:44:17 +0530

    crateBillingAccount service from the BillingServiceImplBase

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,7 +1,6 @@
 package com.pm.billingservice.grpc;
 
-import billing.BillingRequest;
-import billing.BillingResponse;
+
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -10,29 +9,9 @@ import net.devh.boot.grpc.server.service.GrpcService;
 public class BillingGrpcService extends BillingServiceImplBase {
 
     @Override
-    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
-        try {
-            // logic for patient billing account creation
-            String patientId = request.getPatientId();
-            double amount = request.getAmount();
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseObserver) {
 
-            // save billing info to DB here
-            System.out.println("Creating billing account for Patient ID: " + patientId + " with amount: " + amount);
-
-            // Build response
-            BillingResponse response = BillingResponse.newBuilder()
-                    .setStatus("SUCCESS")
-                    .setMessage("Billing account created for patient " + patientId)
-                    .setPatientId(patientId)
-                    .setAmount(amount)
-                    .build();
-
-            // Send response
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            // Handle error
-            responseObserver.onError(e);
-        }
     }
+
+
 }
