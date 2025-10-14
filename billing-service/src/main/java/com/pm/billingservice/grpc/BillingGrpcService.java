838883825b7bef656e0cb33801d9abfcc27e4d6e
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-10-14 16:01:00 +0530

    Update BillingGrpcService.java

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
 
-    // Logger for logging request and response details
+    
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
     /**
